944bd5b6afd96eb97984e34812bfb2c72246bf73
lorry <ly3124675luces@163.com>
2022-11-18 22:56:00 +0800

    modified:   src/App.vue 	new file:   src/assets/index.png 	new file:   src/assets/login/back.jpg 	new file:   src/assets/login/back.png 	new file:   src/assets/login/background.jpg 	new file:   src/assets/login/web5.jpg 	new file:   src/components/FindUser.vue 	modified:   src/components/HelloWorld.vue 	new file:   src/components/OfficialIndex.vue 	new file:   src/components/form/LoginForm.vue 	new file:   src/components/form/RegisterForm.vue 	new file:   src/components/navBar/NavBar.vue 	modified:   src/main.js 	modified:   src/router/index.js 	new file:   src/style/common.less 	new file:   src/style/mixin.less 	new file:   src/views/IndexView.vue 	new file:   src/views/LoginView.vue 	new file:   src/views/RegisterView.vue

---
+++ src/components/navBar/NavBar.vue
@@ -0,0 +1,103 @@
+<template>
+  <!-- <el-menu
+    :default-active="this.$router.path"
+    class="el-menu-demo"
+    mode="horizontal"
+    :ellipsis="false"
+    router
+  >
+    <div class="flex-grow" />
+    <el-menu-item index="/">我们主页</el-menu-item>
+    <div class="flex-grow" />
+    <el-sub-menu index="2">
+      <template #title>产品介绍</template>
+      <el-menu-item index="2-1">item one</el-menu-item>
+      <el-menu-item index="2-2">item two</el-menu-item>
+      <el-menu-item index="2-3">item three</el-menu-item>
+      <el-sub-menu index="2-4">
+        <template #title>item four</template>
+        <el-menu-item index="2-4-1">item one</el-menu-item>
+        <el-menu-item index="2-4-2">item two</el-menu-item>
+        <el-menu-item index="2-4-3">item three</el-menu-item>
+      </el-sub-menu>
+    </el-sub-menu>
+    <el-sub-menu index="3">
+      <template #title>产品支持</template>
+      <el-menu-item index="3-1">item one</el-menu-item>
+      <el-menu-item index="3-2">item two</el-menu-item>
+      <el-menu-item index="3-3">item three</el-menu-item>
+    </el-sub-menu>
+    <div class="flex-grow" />
+    <el-menu-item index="3" disabled>打开控制台</el-menu-item>
+    <el-menu-item index="/login">登录</el-menu-item>
+    <div class="flex-grow" />
+  </el-menu> -->
+  <el-menu router :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+    <el-menu-item index="/" key="1" class="index_item">我的主页</el-menu-item>
+    <el-submenu index="2" key="2" class="product_item">
+      <template slot="title">产品列表</template>
+      <el-menu-item index="2-1">产品1</el-menu-item>
+      <el-menu-item index="2-2">产品2</el-menu-item>
+      <el-menu-item index="2-3">产品3</el-menu-item>
+    </el-submenu>
+    <el-submenu index='3' key="3" class="intro_item">
+      <template slot="title">关于我们</template>
+      <el-menu-item index="2-4-1">团队介绍</el-menu-item>
+      <el-menu-item index="2-4-2">打入我们</el-menu-item>
+    </el-submenu>
+    <el-menu-item index="3" disabled class="control_item">控制台</el-menu-item>
+    <el-menu-item index="/login" class="login_item">登录</el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '/',
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        this.activeIndex = key
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.flex-grow {
+  flex-grow: 1;
+}
+
+.el-menu-demo{
+  position:absolute;
+  left: 0;
+  top: 0;
+  float: left;
+  width: 100%;
+  z-index: 1;
+  background: transparent;
+}
+
+.index_item{
+  margin-left: 15% !important;
+}
+
+.product_item{
+  margin-left: 20% !important;
+}
+
+.intro_item {
+  margin-left: 2% !important;
+}
+
+.control_item {
+  margin-left: 20% !important;
+}
+
+.login_item{
+  margin-left: 2% !important;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import IndexView from '../views/IndexView.vue'
+import AboutView from '../views/AboutView.vue'
+import LoginView from '../views/LoginView.vue'
 import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
@@ -7,16 +10,23 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
+    name: 'index',
+    component: IndexView
+  },
+  {
+    path: '/home',
     name: 'home',
     component: HomeView
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: AboutView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
   }
 ]
 
